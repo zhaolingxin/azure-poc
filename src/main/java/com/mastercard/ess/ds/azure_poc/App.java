c7b3729b878b22a5b790bc4a4a3e398b6860ecfc
Zhao, Lingxin <Lingxin.Zhao@mastercard.com>
2020-01-13 16:08:13 -0600

    initial commit

---
+++ src/main/java/com/mastercard/ess/ds/azure_poc/App.java
@@ -0,0 +1,13 @@
+package com.mastercard.ess.ds.azure_poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
