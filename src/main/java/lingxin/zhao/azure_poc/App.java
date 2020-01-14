ef04daaec3845a06f7c9b63d78cf05ddff52bb89
Lingxin Zhao <zhaolingxin@wustl.edu>
2020-01-14 10:56:51 -0600

    Update App.java

--- src/main/java/lingxin/zhao/azure_poc/App.java
+++ src/main/java/lingxin/zhao/azure_poc/App.java
@@ -1,13 +1,21 @@
 package lingxin.zhao.azure_poc;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 /**
  * Hello world!
  *
  */
+
+
+@ConfigurationProperties 
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		SpringApplication.run(App.class, args);
     }
 }
